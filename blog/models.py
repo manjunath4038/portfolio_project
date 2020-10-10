ba08de3b9b49e8b720fe79a5e1495a5cd91ea40b
manjunath4038 <manjunath.batakurki38@gmail.com>
2020-10-10 20:00:49 +0530

    blog work also done

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+    
+    def __str__(self):
+        return self.title
+    
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
     
