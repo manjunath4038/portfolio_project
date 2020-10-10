e12567316924b44bf199b1f6e6df55974a42b3db
manjunath4038 <manjunath.batakurki38@gmail.com>
2020-10-10 16:18:28 +0530

    Jobs are setup now

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-
+class Blog(models.Model):
+    """
+    docstring
+    """
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    
